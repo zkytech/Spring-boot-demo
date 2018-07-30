b25a3b9e05741e6ce09390d91e37d4890f740346
root <zhangkunyuan@hotmail.com>
2018-07-31 00:16:49 +0800

    v1.2

--- src/main/java/com/zkytech/Autowired_Example/test.java
+++ src/main/java/com/zkytech/Autowired_Example/test.java
@@ -2,9 +2,11 @@ package com.zkytech.Autowired_Example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConfigurationProperties
 public class test implements CommandLineRunner {
     @Autowired
     school school1;
